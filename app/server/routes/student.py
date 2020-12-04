ab3ca7959e511f26fc5008d878a2dfe0f48f325a
dwraj <dhinesh21raj@gmail.com>
2020-12-04 14:03:57 -0700

    CRUD Routes done

--- app/server/routes/student.py
+++ app/server/routes/student.py
@@ -24,3 +24,18 @@ async def add_student_data(student : StudentSchema = Body(...)):
   student = jsonable_encoder(student)
   new_student = await add_student(student)
   return ResponseModel(new_student, "Student added successfully")
+
+@router.get("/", response_description = "all students retrieved")
+async def get_students():
+  students = await retrieve_students()
+  if students:
+    return ResponseModel(students, "Student data retrieved")
+  return ErrorrResponseModel("An error occured", 404, "Students don't exist")
+
+#handling a get request with an ID as the parameter
+@router.get("/{id}", response_description = "Single student retrieved")
+async def get_student_data(id):
+  student = await retrieve_student(id)
+  if student:
+    return ResponseModel(student, "student data retrieved")
+  return ErrorrResponseModel("An error occured", 404, "Student doesn't exist")
