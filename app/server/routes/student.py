8176626b3d47323ab4575e8aff21aab220401fe7
dwraj <dhinesh21raj@gmail.com>
2020-12-04 14:13:41 -0700

    CRUD Routes done

--- app/server/routes/student.py
+++ app/server/routes/student.py
@@ -39,3 +39,22 @@ async def get_student_data(id):
   if student:
     return ResponseModel(student, "student data retrieved")
   return ErrorrResponseModel("An error occured", 404, "Student doesn't exist")
+
+@router.put("/{id}")
+async def update_student_data(id: str, req: UpdateStudentModel = Body(...)):
+  req = {k: v for k,v in req.dict().items() if v is not None}
+  updated_student = await update_student(id, req)
+  if updated_student:
+    return ResponseModel(
+      "Student with ID {} has been updated".format(id),
+      "Student name updated succcesfully"
+    )
+  return ErrorrResponseModel("An error occured", 404, "Error updating student data")
+
+router.delete("/{id}", response_description = "Student data deleted from the database")
+async def delete_student_data(id: str):
+  deleted_student = await delete_student(id)
+  if deleted_student:
+    return ResponseModel( "Student with ID: {} removed".format(id), "Student deleted succesfully")
+  else
+    return ErrorrResponseModel("An error occurred", 404, "Student with id {0} doesn't exist".format(id))
